08c1f2585456af4a6eab263a52963d0f7049ab0a
jeongyeonjae0 <jeongyeonjae0@gamil.com>
2025-09-18 15:05:51 +0900

    250918 pwa 적용

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,11 @@
 import { useNavigate } from 'react-router-dom';
-import titleImg from '../assets/andong_tal.png';
 import './Main.css';
 
 function Main() {
   const navigate = useNavigate();
   return (
     <>
-      <img className='title-img' onClick={() => { navigate('/festivals') }} src={titleImg} alt="대문" />
+      <img className='title-img' onClick={() => { navigate('/festivals') }} src='/base/andong_tal.png' alt="대문" />
     </>
   )
 }
